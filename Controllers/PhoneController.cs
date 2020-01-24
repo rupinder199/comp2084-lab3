7c605013fca5fbe71b4fbf0d8945c6a3ddf9c4ff
Rupinderjeet Singh <200433751@student.georgianc.on.ca>
2020-01-23 20:54:55 -0400

    Completed Lab 3

---
+++ Controllers/PhoneController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhoneApplication.Models;
+namespace PhoneApplication.Controllers
+{
+    public class PhoneController : Controller
+    {
+        // GET: Phone
+        public ActionResult Index()
+        {
+            var phoneList = new List<Phone>
+           {
+               new Phone { PhoneId = 01 ,PhoneName = "Samsung",Manufacturer = "Samsung", DateReleased = DateTime.Now },
+               new Phone {PhoneId = 02 , PhoneName = "Razor",Manufacturer = "Razor", DateReleased = DateTime.Now},
+               new Phone {PhoneId = 03, PhoneName = "Rog", Manufacturer = "Asus", DateReleased = DateTime.Now},
+           };
+            return View(phoneList);
+          
+        }
+    }
+}
---
+++ Models/Phone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneApplication.Models
+{
+    public class Phone
+    {
+        public Phone()
+        {
+            PhoneName = "";
+            Manufacturer = "";
+            DateReleased = DateTime.Now;
+        }
+
+        public int PhoneId { get; set; }
+        public string PhoneName { get; set; }
+        public string Manufacturer { get; set; }
+        public DateTime DateReleased { get; set; }
+    }
+}
